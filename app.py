4f0570b20b6010f9d3e146a87528ec036646c7d7
Özgür Akın <ozgurakn14@gmail.com>
2024-05-14 21:23:58 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import time
+
+cred = credentials.Certificate('gossipai-server-firebase-adminsdk-rasja-b50b600f22.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def create_document():
+    news_ref = db.collection('news').document()
+    news_ref.set({})
+    print("Created a new document in 'news' collection.")
+
+while True:
+    create_document()
+    time.sleep(60)
