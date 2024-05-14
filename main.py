3f1ec355582833d8cbd6bb14fc88c6dc155ffe3b
Özgür Akın <ozgurakn14@gmail.com>
2024-05-14 22:10:09 +0300

    update news doc creation

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def create_document():
-    news_ref = db.collection('news').document()
 
     url = "https://newsapi.ai/api/v1/minuteStreamArticles?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22sourceLocationUri%22%3A%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FUnited_States%22%7D%2C%7B%22lang%22%3A%22eng%22%7D%5D%7D%2C%22%24filter%22%3A%7B%22isDuplicate%22%3A%22skipDuplicates%22%7D%7D&apiKey=15612acc-18f1-4014-83a2-db4d45297aa7&callback=JSON_CALLBACK"
 
@@ -21,6 +20,7 @@ def create_document():
 
     # Save the news to Firestore
     for article in news["recentActivityArticles"]["activity"]:
+        news_ref = db.collection('news').document()
         news_ref.set(article)
 
     print("Created a new document in 'news' collection.")
