4c7e39659ce851470736b07f0eeffd6b52be3e1e
Özgür Akın <ozgurakn14@gmail.com>
2024-06-02 04:01:27 +0300

    fix query body

--- news_request.py
+++ news_request.py
@@ -23,21 +23,23 @@ request_body = {
 }
 
 request_body_alt = {
-  "$query": {
-    "$and": [
-      {
-        "sourceLocationUri": "http://en.wikipedia.org/wiki/United_States"
-      },
-      {
-        "lang": "eng"
-      }
-    ]
-  },
-  "$filter": {
-    "hasEvent": "skipArticlesWithoutEvent",
-    "startSourceRankPercentile": 70,
-    "endSourceRankPercentile": 100,
-    "isDuplicate": "skipDuplicates"
-  },
-  "apiKey": os.getenv("NEWS_API_KEY")
+    "query": {
+        "$query": {
+            "$and": [
+                {
+                    "sourceLocationUri": "http://en.wikipedia.org/wiki/United_States"
+                },
+                {
+                    "lang": "eng"
+                }
+            ]
+        },
+        "$filter": {
+            "hasEvent": "skipArticlesWithoutEvent",
+            "startSourceRankPercentile": 70,
+            "endSourceRankPercentile": 100,
+            "isDuplicate": "skipDuplicates"
+        }
+    },
+    "apiKey": os.getenv("NEWS_API_KEY")
 }
